8c2eb1a174f05a983860ca7e64c9418efd188b5c
Savan-Gadhiya <gadhiyasavan001@gmail.com>
2021-12-13 13:30:11 +0530

    Minore Change

--- frontend/src/Components/Utility_Component/Footer.js
+++ frontend/src/Components/Utility_Component/Footer.js
@@ -11,16 +11,16 @@ const Footer = () => {
                           <li><a href="#">about us</a></li>
                           <li><a href="#">our services</a></li>
                           <li><a href="#">privacy policy</a></li>
-                          <li><a href="#">affiliate program</a></li>
+                          <li><a href="#">contact us</a></li>
                       </ul>
                   </div>
                   <div className="footer-col">
                       <h4>Get help</h4>
                       <ul>
                           <li><a href="#">FAQ</a></li>
-                          <li><a href="#">shipping</a></li>
-                          <li><a href="#">returns</a></li>
-                          <li><a href="#">paymant options</a></li>
+                          <li><a href="#">contact us</a></li>
+                          <li><a href="#">chat with us</a></li>
+                          {/* <li><a href="#">paymant options</a></li> */}
                       </ul>
                   </div>
                   <div className="footer-col">
